d7c573ac8306b3aa9f469ccec4d96ae3df77c968
Aidyn Yerdibayev <yerdibayev@gmail.com>
2024-02-17 13:07:28 +0600

    docs added

--- app/Http/Controllers/API/FeedbackController.php
+++ app/Http/Controllers/API/FeedbackController.php
@@ -17,6 +17,14 @@ class FeedbackController extends Controller
         $this->service = $feedbackService;
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/feedbacks",
+     *     summary="Get feedbacks",
+     *     description="List of feedbacks",
+     *     @OA\Response(response="200", description="Get feedbacks")
+     * )
+     */
     public function index()
     {
         $data = Feedback::all();
@@ -26,11 +34,61 @@ class FeedbackController extends Controller
         ]);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/feedbacks",
+     *     summary="Creates a feedback",
+     *     description="Creates a feedback",
+     *     @OA\Parameter(
+     *          name="name",
+     *          in="query",
+     *          description="Name (required)",
+     *          @OA\Schema(type="string")
+     *      ),
+     *      @OA\Parameter(
+     *          name="email",
+     *          in="query",
+     *          description="Email (required)",
+     *          @OA\Schema(type="string")
+     *      ),
+     *     @OA\Parameter(
+     *           name="phone",
+     *           in="query",
+     *           description="Phone (nullable)",
+     *           @OA\Schema(type="string")
+     *       ),
+     *     @OA\Parameter(
+     *           name="city",
+     *           in="query",
+     *           description="City (nullable)",
+     *           @OA\Schema(type="string")
+     *       ),
+     *     @OA\Parameter(
+     *            name="subject",
+     *            in="query",
+     *            description="Subject (nullable)",
+     *            @OA\Schema(type="string")
+     *        ),
+     *     @OA\Parameter(
+     *             name="message",
+     *             in="query",
+     *             description="Message (nullable)",
+     *             @OA\Schema(type="string")
+     *         ),
+     *     @OA\Parameter(
+     *             name="file",
+     *             in="query",
+     *             description="File (nullable)",
+     *             @OA\Schema(type="file")
+     *         ),
+     *     @OA\Response(response="201", description="Create feedback")
+     * )
+     */
     public function store(Request $request)
     {
         $feedbackDTO = new FeedbackDTO($request->all());
         $this->service->saveFeedback($feedbackDTO);
 
-        return response()->json(['message' => 'Message sent!']);
+        return response()->json(['message' => 'Message sent!'], 201);
     }
 }
